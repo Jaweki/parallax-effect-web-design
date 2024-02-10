bf1ad0ebff21d48943663743397a62f1e9f346b3
Jaweki <werukioni@gmail.com>
2024-02-10 19:34:26 +0300

    corrected page layout issue

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,11 +1,8 @@
 function HeroSection() {
   return (
     <div className="fixed top-0">
-
-      <img src={"./images/mobile/image-hero.jpg"} className="w-screen sm:hidden " alt="" />
-      <img src={"./images/desktop/image-hero.jpg"} className="w-screen max-sm:hidden -z-10" alt="" />
-
-
+      <img src={"./images/mobile/image-hero.jpg"} className=" object-cover w-full sm:hidden " alt="" />
+      <img src={"./images/desktop/image-hero.jpg"} className=" object-cover w-full max-sm:hidden" alt="" />
     </div>
   )
 }
--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -2,7 +2,7 @@ import Header from "./Header"
 
 function MainSection() {
   return (
-    <div className="mx-24 absolute top-0 w-full">
+    <div className=" absolute top-0 w-full px-32">
       <Header/>
       <p className="border border-solid border-white absolute top-[200px] p-7 text-white text-5xl font-thin w-[400px] font-josefin z-50">IMMERSIVE EXPERIENCES THAT DELIVER</p>
     </div>
